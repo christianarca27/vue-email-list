4cd60a207b426e85272f02999f28d2ae9a6c1019
christianarca27 <christian.arca.mail@gmail.com>
2023-03-22 15:44:19 +0100

    feat: non funzionante

--- js/script.js
+++ js/script.js
@@ -8,10 +8,20 @@ const { createApp } = Vue
     },
 
     created() {
+        const newEmailList = [];
+
         for(let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
-                this.emailList.push(res.data.response);
+                // this.emailList.push(res.data.response);
+                newEmailList.push(res.data.response);
+                // console.log(newEmailList[i]);
             });
         }
+        console.log(newEmailList);
+
+        // const arrayProva = ["uno", "due", "tre"];
+        // this.emailList = this.emailList.concat(arrayProva);
+        // this.emailList = this.emailList.concat(newEmailList);
+        // console.log(this.emailList);
     },
   }).mount('#app')
