1a72ffc3c04904059452af7c41493a632d377d19
christianarca27 <christian.arca.mail@gmail.com>
2023-03-22 16:17:09 +0100

    feat: bonus, la lista appare a schermo solo una volta che è piena

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const { createApp } = Vue
+const { createApp } = Vue;
+const numEmail = 10;
 
   createApp({
     data() {
@@ -8,20 +9,21 @@ const { createApp } = Vue
     },
 
     created() {
-        const newEmailList = [];
-
-        for(let i = 0; i < 10; i++) {
+        for(let i = 0; i < numEmail; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
-                // this.emailList.push(res.data.response);
-                newEmailList.push(res.data.response);
-                // console.log(newEmailList[i]);
+                this.emailList.push(res.data.response);
             });
         }
-        console.log(newEmailList);
-
-        // const arrayProva = ["uno", "due", "tre"];
-        // this.emailList = this.emailList.concat(arrayProva);
-        // this.emailList = this.emailList.concat(newEmailList);
-        // console.log(this.emailList);
     },
-  }).mount('#app')
+
+    methods: {
+        isListFull() {
+            if(this.emailList.length >= numEmail) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    },
+  }).mount('#app');
